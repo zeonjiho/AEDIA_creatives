3f36f304f9a7a408ccccbf856ed8c87ab1d2fa7e
DominicKang <On MacBookPro>
2025-04-21 17:07:02 +0900

    b

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors')
+const https = require('https')
+const fs = require('fs')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const sharp = require('sharp')
+const path = require('path')
+const axios = require('axios')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+const port = 8181;
+const tokenSecretKey = 'temp_key';
+
+// const User = require('./models/User')
+// const Category = require('./models/Category')
+// const Review = require('./models/Review')
+// const Product = require('./models/Product')
+// const Order = require('./models/Order')
+// const MainBanner = require('./models/MainBanner')
+// const FreeDesign = require('./models/FreeDesign')
+
+//로컬 버전 http 서버
+app.listen(port, () => {
+    console.log(`\x1b[35mServer is running on port \x1b[32m${port}\x1b[0m ${new Date().toLocaleString()}`);
+})
+
+//배포 버전 https 서버
+// const sslKey = fs.readFileSync('/etc/letsencrypt/live/alpha1024.raulsoft.com/privkey.pem');
+// const sslCert = fs.readFileSync('/etc/letsencrypt/live/alpha1024.raulsoft.com/fullchain.pem');
+// const credentials = { key: sslKey, cert: sslCert };
+// https.createServer(credentials, app).listen(port, () => {
+//     console.log(`\x1b[32mhttps \x1b[35mServer is running on port \x1b[32m${port}\x1b[0m ${new Date().toLocaleString()}`);
+// });
+
+//MongoDB 연결
+mongoose.connect('mongodb+srv://bilvin0709:qyxFXyPck7WgAjVt@cluster0.sduy2do.mongodb.net/aedia')
+    .then(() => {
+        const db = mongoose.connection;
+        console.log(`\x1b[35mMongoDB Connected in \x1b[32m${db.name}\x1b[0m ${new Date().toLocaleString()}`)
+    })
+    .catch((err => console.log(err)))
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// 업로드된, 실제 이미지 서빙을 위한 정적 파일 경로 설정
+app.use('/uploads', express.static('uploads'));
+
+
+
+
+// ----------------------------------------------------------
+// API 라우터
+// ----------------------------------------------------------
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
