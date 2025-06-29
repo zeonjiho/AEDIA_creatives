cd59e876c06db8b6397a64b18026920979116c0b
DominicKang <On MacBookPro>
2025-06-29 23:07:37 +0900

    b

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:8181';
-// const baseURL = 'https://alpha2080.raulsoft.com:8181';
+// const baseURL = 'http://localhost:8181';
+const baseURL = 'https://aedia.app:8181';
 
 export default baseURL;
